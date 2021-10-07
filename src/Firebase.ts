042afd91b6529f7a8db6ffe4287aed3952b0e8d1
Redekian <redekiangaming@gmail.com>
2021-10-07 12:43:33 +0300

    initial commut

---
+++ src/Firebase.ts
@@ -0,0 +1,18 @@
+import * as firebase from "firebase/app";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBJD1mj6n1eXKYZbnzfnQBgNRoj2DrIhBc",
+    authDomain: "redcord-55973.firebaseapp.com",
+    projectId: "redcord-55973",
+    storageBucket: "redcord-55973.appspot.com",
+    messagingSenderId: "432988504234",
+    appId: "1:432988504234:web:b6d282cd297f20355110d3",
+    measurementId: "G-W049D4XTY1",
+};
+
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
+
+export default firebase;
